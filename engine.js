21a81d46d847cc675404d5be5231331e12afdef3
jadewizard <jadewizard@yandex.ru>
2015-10-04 20:56:21 +0300

    отрисовка блоков

--- engine.js
+++ engine.js
@@ -47,6 +47,37 @@ function racket(x, y, width, height)
     }
 }
 
+function block(x,y,oneWidth,oneHeight,row,col,padding)
+{
+    this.x = x;
+    this.y = y;
+    this.w = oneWidth;
+    this.h = oneHeight;
+    this.p = padding;
+
+    block = new Array(row);
+
+    for(i = 0; i < row; i++)
+    {
+        for(j = 0; j < col; j++)
+        {
+            block[i,j] = 1;
+        }
+    }
+
+    this.draw = function (color)
+    {
+        for(i = 0; i < row; i++)
+        {
+            for(j = 0; j < col; j++)
+            {
+                context.fillStyle = color;
+                context.fillRect(i * (this.w + this.p), j * (this.h + this.p), this.w, this.h);
+            }
+        }
+    }
+}
+
 function update()
 {
     if (ball.x + ball.r > 480 || ball.x - ball.r < 0)
@@ -80,8 +111,9 @@ function draw()
     {
         racket.x = event.pageX - 100;
     }
-    field.draw("#000", 0.1);
-    ball.draw("#F00", 1);
+    field.draw("#FFF", 0.1);
+    block.draw("#F00");
+    ball.draw("#000", 1);
     racket.draw("#F00",10);
     update();
 }
@@ -92,6 +124,7 @@ function init()
     field = new rect(0,0,480,320);
     ball  = new circle(field.w / 2, field.h / 2, 15);
     racket = new racket(145, 290, 200, 15);
+    block = new block(0, 0, 25, 25, 18, 5, 1,5);
 
     //Задаём скорость
     speedX = 5;
